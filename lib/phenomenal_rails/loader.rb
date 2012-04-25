f3ec40025d2714830b997b111e2812859e70fed9
Thibault Poncelet <thibault.poncelet@lt-servers.be>
2012-04-25 20:42:59 +0200

    Autoload bug

--- lib/phenomenal_rails/loader.rb
+++ lib/phenomenal_rails/loader.rb
@@ -1,19 +1,45 @@
 class PhenomenalRails::Loader
-  def self.load_dir(path)
+
+  def self.autoload_paths(path,paths=[])
+    self.scan_dir(path) do |filepath, entry|
+      if File.directory?(filepath)
+        if (filepath.match(/.*\/controllers/) ||
+          filepath.match(/.*\/models/) ||
+          filepath.match(/.*\/helpers/))
+          puts "Autoload #{filepath}"        
+          paths.push(filepath)
+        end
+        autoload_paths(filepath,paths)        
+      end
+    end
+    paths
+  end
+  
+  def self.scan_dir(path, &block)
     if Dir.exist? path
       Dir.entries(path).each do |entry|
         if entry!="." && entry !=".."
           filepath=File.join(path,entry)
-          if File.file?(filepath) && entry.match(/.*\.rb/)
-            load filepath
-          elsif File.directory?(filepath)
-            load_dir(filepath)
-          end
+          yield(filepath, entry)
         end
       end
     end
   end
-  
+ 
+  def self.load_files(path)
+    self.scan_dir(path) do |filepath, entry|
+      if File.file?(filepath) && entry.match(/.*\.rb/)
+        puts "Load #{filepath}"
+        load filepath
+      elsif File.directory?(filepath)
+        if !(filepath.match(/.*\/controllers/) ||
+          filepath.match(/.*\/models/) ||
+          filepath.match(/.*\/helpers/))
+          load_files(filepath)
+        end
+      end
+    end
+  end
   def self.prepare(loading=false)
     phen_defined_contexts.reverse.each do |context|
       if !context.forgotten && (!context.persistent || !Rails.configuration.cache_classes)
@@ -26,9 +52,9 @@ class PhenomenalRails::Loader
         end
       end
     end
-  
+
     if !Rails.configuration.cache_classes || loading
-      PhenomenalRails::Loader.load_dir("#{Rails.root}/#{PhenomenalRails::PATH}")
+      PhenomenalRails::Loader.load_files(File.expand_path(PhenomenalRails::PATH,Rails.root))
     end
   end
 end
--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -14,8 +14,6 @@ module Dummy
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{config.root}/app/contexts )
-
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
