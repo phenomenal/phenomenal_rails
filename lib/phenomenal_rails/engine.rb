dbb99ce38a30044d929cfa7f8f408dd77b28006a
Loic Vigneron <loic.vigneron@gmail.com>
2012-05-20 16:35:33 +0200

    Use PhenomenalRails:: for engine + Move PATH to phenomenal_rails.rb

--- lib/phenomenal_rails/engine.rb
+++ lib/phenomenal_rails/engine.rb
@@ -1,23 +1,20 @@
-module PhenomenalRails
-  PATH = "app_phenomenal"
-  class Engine < Rails::Engine
-    isolate_namespace PhenomenalRails
-    config.app_middleware.use(PhenomenalRails::Middleware)
-    
-    config.before_configuration do
-      Rails.configuration.autoload_paths += PhenomenalRails::Loader.autoload_paths(File.expand_path(PhenomenalRails::PATH,Rails.root))
-    end
-    
-    config.to_prepare do
-      PhenomenalRails::Loader.prepare(true)
-    end
-    
-    config.after_initialize do
-      ActionController::Base.prepend_view_path(PhenomenalRails::Resolver.instance)
-    end
-    
-    ActionDispatch::Callbacks.before do |*args|
-      PhenomenalRails::Loader.prepare
-    end
+class PhenomenalRails::Engine < Rails::Engine
+  isolate_namespace PhenomenalRails
+  config.app_middleware.use(PhenomenalRails::Middleware)
+  
+  config.before_configuration do
+    Rails.configuration.autoload_paths += PhenomenalRails::Loader.autoload_paths(File.expand_path(PhenomenalRails::PATH,Rails.root))
+  end
+  
+  config.to_prepare do
+    PhenomenalRails::Loader.prepare(true)
+  end
+  
+  config.after_initialize do
+    ActionController::Base.prepend_view_path(PhenomenalRails::Resolver.instance)
+  end
+  
+  ActionDispatch::Callbacks.before do |*args|
+    PhenomenalRails::Loader.prepare
   end
 end
